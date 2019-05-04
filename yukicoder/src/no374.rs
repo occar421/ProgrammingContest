e5a3fe9de00f60d16be5f0b114c3245d48a761c6
MasuqaT <occar@hotmail.co.jp>
2019-05-04 21:48:05 +0900

    yukicoder no374

---
+++ yukicoder/src/no374.rs
@@ -0,0 +1,128 @@
+use std::io;
+use std::io::{BufRead, Write};
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    (stdin = $s:expr, $($r:tt)*) => {
+        let s = {
+            let mut s = String::new();
+            $s.read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_judge {
+    ($method:ident, $input:expr, $expected:expr) => {
+        {
+            let input = $input.as_bytes();
+            let mut output = Vec::new();
+
+            $method(&input[..], &mut output);
+
+            let output = String::from_utf8(output).expect("Not UTF-8");
+
+            assert_eq!($expected, output);
+        }
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_judge_with_output {
+    ($method:ident, $input:expr) => {
+        {
+            let input = $input.as_bytes();
+            let mut output = Vec::new();
+
+            $method(&input[..], &mut output);
+
+            String::from_utf8(output).expect("Not UTF-8")
+        }
+    };
+}
+
+fn main() {
+    let stdio = io::stdin();
+    let input = stdio.lock();
+
+    let output = io::stdout();
+
+    process(input, output);
+}
+
+fn process<R, W>(mut reader: R, mut writer: W) -> () where
+    R: BufRead,
+    W: Write {
+    input! {
+        stdin = reader,
+        r_table: usize, r_coin: usize
+    }
+
+    if r_table < r_coin {
+        // S can't put any coin at first.
+        write!(writer, "K");
+        return;
+    }
+
+//    if r_table * 3 <= r_coin {
+//        // S can put a coin at the center.
+//        write!(writer, "S");
+//        return;
+//    }
+
+    // S always wins?
+    write!(writer, "S");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        assert_judge!(process, "4 6", "K");
+    }
+
+    #[test]
+    fn sample2() {
+        assert_judge!(process, "2048 788", "S");
+    }
+}
