df3d956ade9b8029cad5356b946f59e5c9d673f2
MasuqaT <occar@hotmail.co.jp>
2021-04-12 00:37:37 +0900

    abc198 temp

---
+++ atcoder-practice/temporary/src/abc198_e.rs
@@ -0,0 +1,399 @@
+// Macro by MasuqaT (occar421)
+// https://github.com/occar421/ProgrammingContest/tree/master/templates/src/standard_io.rs
+
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::io;
+use std::io::{BufRead, Result, Write};
+use std::ops::{Div, Mul, Rem};
+use std::rc::Rc;
+use std::str::FromStr;
+
+// From https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    (stdin = $s:expr, $($r:tt)*) => {
+        let s = {
+            let mut s = String::new();
+            $s.read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap_or_default().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_judge {
+    ($method:ident, $input:expr, $expected:expr) => {{
+        let input = $input.as_bytes();
+        let mut output = Vec::new();
+
+        $method(&input[..], &mut output).expect("Should not emit error");
+
+        let mut output = String::from_utf8(output).expect("Not UTF-8");
+
+        if output.ends_with('\n') {
+            output.pop();
+            if output.ends_with('\r') {
+                output.pop();
+            }
+        }
+
+        assert_eq!(output, $expected);
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_judge_with_output {
+    ($method:ident, $input:expr) => {{
+        let input = $input.as_bytes();
+        let mut output = Vec::new();
+
+        $method(&input[..], &mut output).expect("Should not emit error");
+
+        String::from_utf8(output).expect("Not UTF-8")
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_eq_with_error {
+    ($left:expr, $right:expr, $precision:expr) => {{
+        match (&$left, &$right, &$precision) {
+            (left_val, right_val, precision_val) => {
+                if !(*left_val - *precision_val < *right_val
+                    && *right_val < *left_val + *precision_val)
+                {
+                    // The reborrows below are intentional. Without them, the stack slot for the
+                    // borrow is initialized even before the values are compared, leading to a
+                    // noticeable slow down.
+                    panic!(
+                        r#"assertion failed: `(left == right) +- precision`
+      left: `{:?}`,
+     right: `{:?}`,
+ precision: `{:?}`"#,
+                        &*left_val, &*right_val, &*precision_val
+                    )
+                }
+            }
+        }
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_judge_with_error {
+    ($method:ident, $input:expr, $expected:expr, $t:ty | $precision:expr ) => {{
+        let input = $input.as_bytes();
+        let mut output = Vec::new();
+
+        $method(&input[..], &mut output).expect("Should not emit error");
+
+        let output = String::from_utf8(output).expect("Not UTF-8");
+
+        let actual: $t = output.parse().unwrap();
+        let expected: $t = $expected.parse().unwrap();
+
+        assert_eq_with_error!(actual, expected, $precision);
+    }};
+}
+
+pub trait GenericInteger:
+    Copy + PartialEq + FromStr + Display + Rem<Output = Self> + Div<Output = Self> + Mul<Output = Self>
+{
+    fn zero() -> Self;
+}
+
+macro_rules! dec_gi {
+    () => {};
+    ($t:ty $(, $r:ty)*) => {
+        impl GenericInteger for $t {
+            #[inline]
+            fn zero() -> Self { 0 }
+        }
+
+        dec_gi![ $( $r ),* ];
+    };
+}
+
+dec_gi![u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize];
+
+#[allow(dead_code)]
+pub fn gcd<T>(a: T, b: T) -> T
+where
+    T: GenericInteger,
+{
+    if b == T::zero() {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+#[allow(dead_code)]
+#[inline]
+pub fn lcm<T>(a: T, b: T) -> T
+where
+    T: GenericInteger,
+{
+    a / gcd(a, b) * b
+}
+
+pub trait IterExt<T>
+where
+    T: Display,
+{
+    fn easy_join(&mut self, separator: &str) -> String;
+}
+
+impl<TItem, TTrait> IterExt<TItem> for TTrait
+where
+    TItem: Display,
+    TTrait: Iterator<Item = TItem>,
+{
+    #[inline]
+    fn easy_join(&mut self, separator: &str) -> String {
+        self.map(|i| format!("{}", i))
+            .collect::<Vec<_>>()
+            .join(separator)
+    }
+}
+
+pub trait VecExt<T> {
+    fn add_like_string(&mut self) -> T;
+}
+
+impl<T> VecExt<T> for Vec<T>
+where
+    T: GenericInteger,
+{
+    #[inline]
+    fn add_like_string(&mut self) -> T {
+        if let Ok(value) = self.iter().easy_join("").parse::<T>() {
+            value
+        } else {
+            panic!("Invalid value")
+        }
+    }
+}
+
+#[allow(unused_macros)]
+macro_rules! swap {
+    ($v1:expr, $v2:expr) => {
+        let buf = $v1;
+        $v1 = $v2;
+        $v2 = buf;
+    };
+}
+
+#[macro_export]
+macro_rules! invert_index {
+    ($v:expr) => {{
+        let mut goal = vec![0usize; $v.len()];
+        for (i, v) in $v.iter().enumerate() {
+            goal[*v] = i;
+        }
+        goal
+    }};
+}
+
+trait ThenSome: Into<bool> {
+    fn then_some_<T>(self, t: T) -> Option<T> {
+        if self.into() {
+            Some(t)
+        } else {
+            None
+        }
+    }
+}
+
+impl ThenSome for bool {}
+
+fn main() {
+    let stdio = io::stdin();
+    let input = stdio.lock();
+
+    let output = io::stdout();
+
+    process(input, output).expect("Should not emit error");
+}
+
+#[allow(non_snake_case)]
+fn process<R, W>(mut reader: R, mut writer: W) -> Result<()>
+where
+    R: BufRead,
+    W: Write,
+{
+    input! {
+        stdin = reader,
+        n: usize,
+        c: [usize; n],
+        ab: [(usize, usize); n - 1],
+    }
+
+    let c = {
+        let mut new_c = vec![0];
+        new_c.extend(c);
+        new_c
+    };
+
+    // ignore n == 0
+
+    // after checking editor's note
+
+    let mut edges = HashMap::new();
+    for (src, dest) in ab.iter() {
+        edges.entry(*src).or_insert(vec![]).push(*dest);
+        edges.entry(*dest).or_insert(vec![]).push(*src);
+    }
+
+    let n_colors = Rc::new(RefCell::new(HashMap::new()));
+    let arrived_node_flags = Rc::new(RefCell::new(vec![false; n + 1]));
+    let good_nodes = Rc::new(RefCell::new(vec![false; n + 1]));
+
+    fn visit(
+        i: usize,
+        c: &Vec<usize>,
+        edges: &HashMap<usize, Vec<usize>>,
+        n_colors: Rc<RefCell<HashMap<usize, usize>>>,
+        arrived_node_flags: Rc<RefCell<Vec<bool>>>,
+        good_nodes: Rc<RefCell<Vec<bool>>>,
+    ) {
+        {
+            let mut arrived_node_flags = arrived_node_flags.borrow_mut();
+            if arrived_node_flags[i] {
+                return;
+            }
+            arrived_node_flags[i] = true;
+        }
+
+        {
+            let mut n_colors = n_colors.borrow_mut();
+            let n_color = n_colors.entry(c[i]).or_insert(0);
+            if *n_color == 0 {
+                good_nodes.borrow_mut()[i] = true;
+            }
+            *n_color += 1;
+        }
+
+        for &dest in edges.get(&i).expect("c").iter() {
+            visit(
+                dest,
+                c,
+                edges,
+                n_colors.clone(),
+                arrived_node_flags.clone(),
+                good_nodes.clone(),
+            );
+        }
+
+        {
+            let mut n_colors = n_colors.borrow_mut();
+            *n_colors.get_mut(&c[i]).unwrap() -= 1;
+        }
+    }
+
+    visit(
+        1,
+        &c,
+        &edges,
+        n_colors,
+        arrived_node_flags,
+        good_nodes.clone(),
+    );
+
+    for (i, _) in good_nodes
+        .borrow()
+        .iter()
+        .enumerate()
+        .filter(|(_, &is_good)| is_good)
+    {
+        writeln!(writer, "{}", i)?;
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        assert_judge!(
+            process,
+            "6
+2 7 1 8 2 8
+1 2
+3 6
+3 2
+4 3
+2 5",
+            "1
+2
+3
+4
+6"
+        );
+    }
+
+    #[test]
+    fn sample2() {
+        assert_judge!(
+            process,
+            "10
+3 1 4 1 5 9 2 6 5 3
+1 2
+2 3
+3 4
+4 5
+5 6
+6 7
+7 8
+8 9
+9 10",
+            "1
+2
+3
+5
+6
+7
+8"
+        );
+    }
+}
