04ab6f4e917ae36e24767ea8f77e71e63d47333b
MasuqaT <occar@hotmail.co.jp>
2021-06-13 23:51:24 +0900

    result of abc205

---
+++ atcoder-match/AtCoderBeginnerContest205/abc205_d.rs
@@ -0,0 +1,459 @@
+// Macro by MasuqaT (occar421)
+// https://github.com/occar421/ProgrammingContest/tree/master/templates/src/standard_io.rs
+
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::hash::Hash;
+use std::io;
+use std::io::{BufRead, BufWriter, Result, Write};
+use std::ops::{Add, Div, Mul, Rem, Sub};
+use std::str::FromStr;
+
+pub type NodeIndex0Based = usize;
+pub type NodeIndex1Based = usize;
+pub type Quantity = usize;
+pub type Length = usize;
+
+// From https://github.com/tanakh/competitive-rs/blob/d5f51f01a6f85ddbebec4cfcb601746bee727181/src/lib.rs#L1-L92
+//   and modified by this file author
+macro_rules! input_original {
+    (source = $s:expr; $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut _next = || { iter.next().unwrap() };
+        input_inner!{_next, $($r)*}
+    };
+    (stdin = $s:expr; $($r:tt)*) => {
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new($s));
+        let mut _next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{_next, $($r)*}
+    };
+}
+
+#[doc(hidden)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[doc(hidden)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_judge {
+    ($method:ident, $input:expr, $expected:expr) => {{
+        let output = assert_judge_with_output!($method, $input);
+
+        assert_eq!(output, $expected.trim());
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_judge_with_output {
+    ($method:ident, $input:expr) => {{
+        let input = $input.as_bytes();
+        let mut output = Vec::new();
+
+        $method(&input[..], &mut output).expect("Should not emit error");
+
+        String::from_utf8(output)
+            .expect("Not UTF-8")
+            .trim()
+            .to_string()
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_eq_with_error {
+    ($left:expr, $right:expr, $precision:expr) => {{
+        match (&$left, &$right, &$precision) {
+            (left_val, right_val, precision_val) => {
+                if !(*left_val - *precision_val < *right_val
+                    && *right_val < *left_val + *precision_val)
+                {
+                    // The re-borrows below are intentional. Without them, the stack slot for the
+                    // borrow is initialized even before the values are compared, leading to a
+                    // noticeable slow down.
+                    panic!(
+                        r#"assertion failed: `(left == right) +- precision`
+      left: `{:?}`,
+     right: `{:?}`,
+ precision: `{:?}`"#,
+                        &*left_val, &*right_val, &*precision_val
+                    )
+                }
+            }
+        }
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! assert_judge_with_error {
+    ($method:ident, $input:expr, $expected:expr, $t:ty | $precision:expr ) => {{
+        let output = assert_judge_with_output!($method, $input);
+
+        let actual: $t = output.parse().unwrap();
+        let expected: $t = $expected.parse().unwrap();
+
+        assert_eq_with_error!(actual, expected, $precision);
+    }};
+}
+
+pub trait GenericInteger:
+    Copy
+    + Clone
+    + Eq
+    + PartialEq
+    + Hash
+    + FromStr
+    + Display
+    + Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Div<Output = Self>
+    + Rem<Output = Self>
+{
+    fn zero() -> Self;
+    fn one() -> Self;
+}
+
+macro_rules! implement_generic_integer {
+    () => {};
+    ($t:ty $(, $r:ty)*) => {
+        impl GenericInteger for $t {
+            #[inline]
+            fn zero() -> Self { 0 }
+
+            #[inline]
+            fn one() -> Self { 1 }
+        }
+
+        implement_generic_integer![ $( $r ),* ];
+    };
+}
+
+implement_generic_integer![u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize];
+
+#[allow(dead_code)]
+pub fn gcd<T>(a: T, b: T) -> T
+where
+    T: GenericInteger,
+{
+    if b == T::zero() {
+        a
+    } else {
+        gcd(b, a % b.clone())
+    }
+}
+
+#[allow(dead_code)]
+#[inline]
+pub fn lcm<T>(a: T, b: T) -> T
+where
+    T: GenericInteger,
+{
+    a / gcd(a.clone(), b) * b.clone()
+}
+
+#[allow(dead_code)]
+pub fn prime_factorize(n: usize) -> HashMap<usize, Quantity> {
+    let mut map = HashMap::new();
+
+    let sqrt_n = (n as f64).sqrt().ceil() as usize;
+
+    let mut n = n;
+    for p in 2..=sqrt_n {
+        if n % p != 0 {
+            continue;
+        }
+
+        let mut exp_number = 0;
+        while n % p == 0 {
+            exp_number += 1;
+            n /= p;
+        }
+
+        map.insert(p, exp_number);
+    }
+
+    if n != 1 {
+        map.insert(n, 1);
+    }
+
+    map
+}
+
+pub trait IterExt<T>
+where
+    T: Display,
+{
+    fn easy_join(&mut self, separator: &str) -> String;
+}
+
+impl<TItem, TTrait> IterExt<TItem> for TTrait
+where
+    TItem: Display,
+    TTrait: Iterator<Item = TItem>,
+{
+    #[inline]
+    fn easy_join(&mut self, separator: &str) -> String {
+        self.map(|i| format!("{}", i))
+            .collect::<Vec<_>>()
+            .join(separator)
+    }
+}
+
+pub trait VecExt<T> {
+    fn add_like_string(&mut self) -> T;
+}
+
+impl<T> VecExt<T> for Vec<T>
+where
+    T: GenericInteger,
+{
+    #[inline]
+    fn add_like_string(&mut self) -> T {
+        if let Ok(value) = self.iter().easy_join("").parse::<T>() {
+            value
+        } else {
+            panic!("Invalid value")
+        }
+    }
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+macro_rules! swap {
+    ($v1:expr, $v2:expr) => {
+        let buf = $v1;
+        $v1 = $v2;
+        $v2 = buf;
+    };
+}
+
+#[macro_export]
+macro_rules! invert_index {
+    ($v:expr) => {{
+        let mut goal = vec![0usize; $v.len()];
+        for (i, v) in $v.iter().enumerate() {
+            goal[*v] = i;
+        }
+        goal
+    }};
+}
+
+pub trait ThenSome: Into<bool> {
+    fn then_some_<T>(self, t: T) -> Option<T> {
+        if self.into() {
+            Some(t)
+        } else {
+            None
+        }
+    }
+}
+
+impl ThenSome for bool {}
+
+// From https://kuretchi.hateblo.jp/entry/rust_nested_vec
+#[allow(unused_macros)]
+macro_rules! nested_vec {
+  ($e:expr; $n:expr) => {
+    vec![$e; $n]
+  };
+  ($e:expr; $n:expr $(; $m:expr)+) => {
+    vec![nested_vec!($e $(; $m)+); $n]
+  };
+}
+
+// -- end of helpers
+
+fn main() {
+    let stdio = io::stdin();
+    let input = stdio.lock();
+
+    let mut stdout = io::stdout();
+    let output = BufWriter::new(stdout.lock());
+
+    process(input, output).expect("Should not emit error");
+    stdout.flush().unwrap();
+}
+
+#[allow(non_snake_case)]
+fn process<R, W>(reader: R, mut writer: W) -> Result<()>
+where
+    R: BufRead,
+    W: Write,
+{
+    #[allow(unused_macros)]
+    macro_rules! input {
+        ($($r:tt)*) => {
+            input_original! { stdin = reader; $($r)* }
+        };
+    }
+    #[allow(unused_macros)]
+    macro_rules! print {
+        ($($arg:tt)*) => {
+            write!(writer, $($arg)*)?;
+        }
+    }
+    #[allow(unused_macros)]
+    macro_rules! println {
+        () => {
+            writeln!(writer)?;
+        };
+        ($($arg:tt)*) => {
+            writeln!(writer, $($arg)*)?;
+        }
+    }
+
+    {
+        input! {
+            n: Quantity, q: Quantity,
+            a: [usize; n],
+            k: [usize; q],
+        }
+
+        let mut segments: Vec<(usize, usize)> = vec![(0, 0)];
+        for &a_i in a.iter() {
+            if let Some(last) = segments.last() {
+                if last.0 + last.1 == a_i {
+                    let last = segments.last_mut().unwrap();
+                    *last = (last.0, last.1 + 1);
+                } else {
+                    segments.push((a_i, 1))
+                }
+            } else {
+                segments.push((a_i, 1));
+            }
+        } // O(N)
+        let mut acc = 0;
+        for i in 0..segments.len() {
+            let n_v = acc;
+            acc += segments[i].1;
+            segments[i] = (segments[i].0 - n_v, acc);
+        } // O(N)
+          // seg[i] = (q個目, q までで除かれた個数)
+
+        for &k_i in k.iter() {
+            let mut ng = segments.len();
+            let mut known_less = 0;
+            while known_less + 1 < ng {
+                let mid = (known_less + ng) / 2;
+                if segments[mid].0 > k_i {
+                    ng = mid;
+                } else if segments[mid].0 == k_i {
+                    known_less = mid;
+                    break;
+                } else {
+                    known_less = mid;
+                }
+            } // O( log(N) )
+            let result = segments[known_less.min(segments.len() - 1)];
+
+            println!("{}", k_i + result.1);
+        } // O( Q log(N) )
+    } // O( N + Q log(N) )
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        assert_judge!(
+            process,
+            "
+4 3
+3 5 6 7
+2
+5
+3
+",
+            "
+2
+9
+4
+"
+        );
+    }
+
+    #[test]
+    fn sample1a() {
+        assert_judge!(
+            process,
+            "
+5 4
+3 5 6 7 20
+2
+5
+3
+4
+",
+            "
+2
+9
+4
+8
+"
+        );
+    }
+
+    #[test]
+    fn sample2() {
+        assert_judge!(
+            process,
+            "
+5 2
+1 2 3 4 5
+1
+10
+",
+            "
+6
+15
+"
+        );
+    }
+}
---
+++ atcoder-match/AtCoderBeginnerContest205/readme.md
@@ -0,0 +1,5 @@
+AtCoder Beginner Contest 205 at 2021/06/13
+
+| Position | Point | Time |
+|:---:|:---:|:---:|
+| 4325/8775 | 600/2100 | 20:05 |
