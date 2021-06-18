b8c0dbb8fc0ae7e72329c6983eeb8b04b542b052
MasuqaT <occar@hotmail.co.jp>
2021-06-18 22:50:52 +0900

    modify comment in binary search

--- templates/src/snippet_binary_search.rs
+++ templates/src/snippet_binary_search.rs
@@ -14,15 +14,19 @@ pub mod binary_search {
         pub min_true: Option<GI>,
     }
 
-    pub trait BinaryBorderSearch<T, GI: GenericInteger> {
+    pub trait BinaryBorderSearch<Item, GI: GenericInteger> {
         /// O( log(N) )
         /// Should be sorted
-        fn search_true_false_border(&self, predicate: impl Fn(&T) -> bool) -> TFBorderResult<GI>;
+        fn search_true_false_border(&self, predicate: impl Fn(&Item) -> bool)
+            -> TFBorderResult<GI>;
 
         /// O( log(N) )
         /// Should be sorted
         #[inline]
-        fn search_false_true_border(&self, predicate: impl Fn(&T) -> bool) -> FTBorderResult<GI> {
+        fn search_false_true_border(
+            &self,
+            predicate: impl Fn(&Item) -> bool,
+        ) -> FTBorderResult<GI> {
             let result = self.search_true_false_border(|item| !predicate(item));
             FTBorderResult {
                 max_false: result.max_true,
@@ -31,60 +35,28 @@ pub mod binary_search {
         }
     }
 
-    impl<T: Ord> BinaryBorderSearch<T, usize> for [T] {
+    impl<Item> BinaryBorderSearch<Item, usize> for [Item] {
+        /// O( log(N) )
         /// ```
+        /// # use templates::snippet_binary_search::binary_search::BinaryBorderSearch;
         /// let vec = vec![1, 2, 3];
         /// let result = vec.search_true_false_border(|&x| x > 1);
         /// assert_eq!(result.max_true, 0);
         /// assert_eq!(result.min_false, 1);
         /// ```
+        #[inline]
         fn search_true_false_border(
             &self,
-            predicate: impl Fn(&T) -> bool,
+            predicate: impl Fn(&Item) -> bool,
         ) -> TFBorderResult<usize> {
-            if self.is_empty() {
-                return TFBorderResult {
-                    max_true: None,
-                    min_false: None,
-                };
-            }
-
-            let first = &self[0];
-            if !predicate(first) {
-                return TFBorderResult {
-                    max_true: None,
-                    min_false: 0.into(),
-                };
-            }
-
-            let mut known_max_true = 0;
-            let mut known_min_false = self.len();
-            while known_min_false - known_max_true > 1 {
-                let mid = known_max_true + (known_min_false - known_max_true) / 2;
-                if predicate(&self[mid]) {
-                    known_max_true = mid;
-                } else {
-                    known_min_false = mid;
-                }
-            }
-
-            // all true
-            if known_min_false == self.len() {
-                TFBorderResult {
-                    max_true: (self.len() - 1).into(),
-                    min_false: None,
-                }
-            } else {
-                TFBorderResult {
-                    max_true: known_max_true.into(),
-                    min_false: known_min_false.into(),
-                }
-            }
+            (0..self.len()).search_true_false_border(|&i| predicate(&self[i]))
         }
     }
 
     impl<GI: GenericInteger> BinaryBorderSearch<GI, GI> for Range<GI> {
+        /// O( log(high-low) )
         /// ```
+        /// # use templates::snippet_binary_search::binary_search::BinaryBorderSearch;
         /// let range = 1..4;
         /// let result = range.search_true_false_border(|&x| x <= 1);
         /// assert_eq!(result.max_true, 1);
@@ -141,7 +113,9 @@ pub mod binary_search {
     }
 
     impl<GI: GenericInteger> BinaryBorderSearch<GI, GI> for RangeInclusive<GI> {
+        /// O( log(high-low) )
         /// ```
+        /// # use templates::snippet_binary_search::binary_search::BinaryBorderSearch;
         /// let range = -1..=1;
         /// let result = range.search_true_false_border(|&x| x <= 1);
         /// assert_eq!(result.max_true, 1);
