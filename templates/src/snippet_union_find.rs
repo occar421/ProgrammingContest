1a8012976c610381b477a3db622cdc71c967b477
MasuqaT <occar@hotmail.co.jp>
2021-06-20 15:39:16 +0900

    add UnionFindMapped

--- templates/src/snippet_union_find.rs
+++ templates/src/snippet_union_find.rs
@@ -13,13 +13,15 @@ pub mod union_find {
     //! https://github.com/occar421/ProgrammingContest/tree/master/templates/src/snippet_union_find.rs
 
     use super::{NodeIndex0Based, Quantity};
+    use std::collections::HashSet;
     use std::fmt::Debug;
+    use std::hash::Hash;
 
     pub trait UnionFind<T>: Debug {
         fn get_root_of(&self, node: T) -> Option<T>;
         fn get_size_of(&self, node: T) -> Option<Quantity>;
         fn connect_between(&mut self, a: T, b: T) -> Option<bool>;
-        fn get_roots(&self) -> Vec<T>;
+        fn get_roots(&self) -> Vec<T>; // FIXME Iter
 
         #[inline]
         fn union(&mut self, a: T, b: T) -> Option<bool> {
@@ -33,6 +35,12 @@ pub mod union_find {
 
     pub fn new(n: Quantity) -> Box<dyn UnionFind<NodeIndex0Based>> {
         Box::new(plain::UnionFindPlain::new(n))
+    }
+
+    pub fn new_from_set<'s, T: 'static + Hash + Eq + Debug + Copy /* FIXME */>(
+        set: &'s HashSet<T>,
+    ) -> Box<dyn UnionFind<T>> {
+        Box::new(mapped::UnionFindMapped::new_from_set(set))
     }
 
     mod plain {
@@ -109,6 +117,71 @@ pub mod union_find {
                         Node::RootWithSize(_) => Some(i),
                         Node::ChildrenWithParent(_) => None,
                     })
+                    .collect()
+            }
+        }
+    }
+
+    mod mapped {
+        use super::super::NodeIndex0Based;
+        use super::plain::UnionFindPlain;
+        use super::UnionFind;
+        use crate::standard_io::Quantity;
+        use std::collections::{HashMap, HashSet};
+        use std::fmt::{Debug, Formatter};
+        use std::hash::Hash;
+        use std::iter::FromIterator;
+
+        pub struct UnionFindMapped<T> {
+            core: UnionFindPlain,
+            map: HashMap<T, NodeIndex0Based>,
+            r_map: HashMap<NodeIndex0Based, T>,
+        }
+
+        impl<T: Hash + Eq + Debug + Copy /* FIXME */> UnionFindMapped<T> {
+            pub fn new_from_set(set: &HashSet<T>) -> Self {
+                let labelled_values = set.iter().enumerate();
+                UnionFindMapped {
+                    core: UnionFindPlain::new(set.len()),
+                    map: HashMap::from_iter(labelled_values.clone().map(|(i, &x)| (x, i))),
+                    r_map: HashMap::from_iter(labelled_values.map(|(i, &x)| (i, x))),
+                }
+            }
+        }
+
+        impl<T: Hash + Eq + Debug> Debug for UnionFindMapped<T> {
+            fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+                writeln!(f, "UnionFindMapped {{")?;
+                writeln!(f, "  map: {:?}", self.map)?;
+                writeln!(f, "  r_map: {:?}", self.r_map)?;
+                writeln!(f, "  core: {:?}", self.core)?;
+                writeln!(f, "}}")
+            }
+        }
+
+        impl<T: Hash + Eq + Debug + Copy /* FIXME*/> UnionFind<T> for UnionFindMapped<T> {
+            fn get_root_of(&self, node: T) -> Option<T> {
+                let core_node = *self.map.get(&node)?;
+                let core_root = self.core.get_root_of(core_node)?;
+                Some(self.r_map[&core_root])
+            }
+
+            fn get_size_of(&self, node: T) -> Option<Quantity> {
+                let core_node = *self.map.get(&node)?;
+                self.core.get_size_of(core_node)
+            }
+
+            fn connect_between(&mut self, a: T, b: T) -> Option<bool> {
+                let core_a = *self.map.get(&a)?;
+                let core_b = *self.map.get(&b)?;
+                self.core.connect_between(core_a, core_b)
+            }
+
+            fn get_roots(&self) -> Vec<T> {
+                self.core
+                    .get_roots()
+                    .iter()
+                    .map(|core_root| self.r_map[core_root])
                     .collect()
             }
         }
--- templates/tests/snippet_union_find.rs
+++ templates/tests/snippet_union_find.rs
@@ -1,18 +1,20 @@
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+    use std::iter::FromIterator;
     use templates::snippet_union_find::union_find;
     use test_case::test_case;
 
     #[test_case(1 => vec![0])]
     #[test_case(2 => vec![0, 1])]
     #[test_case(5 => vec![0, 1, 2, 3, 4])]
-    fn check_initial(size: usize) -> Vec<usize> {
+    fn plain_check_initial(size: usize) -> Vec<usize> {
         let uf = union_find::new(size);
         uf.get_roots()
     }
 
     #[test]
-    fn connect() {
+    fn plain_connect() {
         let mut uf = union_find::new(5);
         uf.connect_between(0, 1);
         uf.connect_between(2, 3);
@@ -27,10 +29,44 @@ mod tests {
         assert_ne!(uf.get_root_of(3), uf.get_root_of(4));
     }
 
+    #[test_case(vec![0])]
+    #[test_case(vec![0, 1])]
+    #[test_case(vec![0, 1, 2, 3, 4])]
+    fn mapped_check_initial(values: Vec<usize>) {
+        let set = HashSet::from_iter(values);
+        let uf = union_find::new_from_set(&set);
+        dbg!(&set, &uf);
+        let ac_set = HashSet::from_iter(uf.get_roots());
+
+        assert_eq!(set, ac_set);
+    }
+
+    #[test]
+    fn mapped_connect() {
+        let data = HashSet::from_iter(vec![-4, -2, 0, 1, 3]);
+        let mut uf = union_find::new_from_set(&data);
+        uf.connect_between(-4, -2);
+        uf.connect_between(0, 1);
+        uf.connect_between(-4, 3);
+
+        assert_eq!(uf.get_root_of(-4), uf.get_root_of(-2));
+        assert_eq!(uf.get_root_of(0), uf.get_root_of(1));
+        assert_eq!(uf.get_root_of(-4), uf.get_root_of(3));
+        assert_eq!(uf.get_root_of(-2), uf.get_root_of(3));
+
+        assert_ne!(uf.get_root_of(-4), uf.get_root_of(0));
+        assert_ne!(uf.get_root_of(1), uf.get_root_of(3));
+    }
+
+    // TODO mapped + &str
+
     #[allow(dead_code)]
     fn compiles_with_debug() {
         let uf = union_find::new(5);
+        dbg!(uf);
 
+        let set = HashSet::from_iter(vec!["a"]);
+        let uf = union_find::new_from_set(&set);
         dbg!(uf);
     }
 }
