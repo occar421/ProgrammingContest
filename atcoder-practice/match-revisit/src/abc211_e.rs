fefd24a0f88cba636bfc22ba82b3e93c59500e16
MasuqaT <occar@hotmail.co.jp>
2021-08-07 14:38:34 +0900

    revisit abc211_e

--- atcoder-practice/match-revisit/src/abc211_e.rs
+++ atcoder-practice/match-revisit/src/abc211_e.rs
@@ -590,6 +590,145 @@ impl<T: PartialOrd> Ord for Total<T> {
     }
 }
 
+pub mod adjacent {
+    //! Adjacent
+    //! https://github.com/occar421/ProgrammingContest/tree/master/templates/src/snippet_adjacent.rs
+
+    use std::ops::Range;
+
+    pub struct Adjacent2d<I> {
+        current: (isize, isize),
+        vertical_range: Range<isize>,
+        horizontal_range: Range<isize>,
+        iter: I,
+    }
+
+    mod adjacent_range {
+        use std::ops::{Bound, Range, RangeBounds, RangeInclusive, RangeTo, RangeToInclusive};
+
+        /// The range that unbounded ends are prohibited.
+        pub trait AdjacentRange: RangeBounds<usize> {
+            fn to_range(&self) -> Range<isize> {
+                let start = match self.start_bound() {
+                    Bound::Unbounded => 0,
+                    Bound::Included(&n) => n,
+                    Bound::Excluded(&n) => n + 1,
+                } as isize;
+                let end = match self.end_bound() {
+                    Bound::Unbounded => panic!("Should not reach"),
+                    Bound::Included(&n) => n + 1,
+                    Bound::Excluded(&n) => n,
+                } as isize;
+
+                start..end
+            }
+        }
+        impl AdjacentRange for RangeTo<usize> {}
+        impl AdjacentRange for RangeToInclusive<usize> {}
+        impl AdjacentRange for Range<usize> {}
+        impl AdjacentRange for RangeInclusive<usize> {}
+        impl AdjacentRange for (Bound<usize>, Bound<usize>) {}
+        impl AdjacentRange for RangeTo<&usize> {}
+        impl AdjacentRange for RangeToInclusive<&usize> {}
+        impl AdjacentRange for Range<&usize> {}
+        impl AdjacentRange for RangeInclusive<&usize> {}
+        impl<'a> AdjacentRange for (Bound<&'a usize>, Bound<&'a usize>) {}
+    }
+
+    impl<'a, I> Adjacent2d<I>
+    where
+        I: Iterator<Item = &'a (isize, isize)>,
+    {
+        /// Arguments
+        ///
+        /// * `current`: (y, x)
+        /// * `vertical_range`: y range
+        /// * `horizontal_range`: x range
+        pub fn new(
+            current: (usize, usize),
+            vertical_range: impl adjacent_range::AdjacentRange,
+            horizontal_range: impl adjacent_range::AdjacentRange,
+            iter: I,
+        ) -> Self {
+            Self {
+                current: (current.0 as isize, current.1 as isize),
+                vertical_range: vertical_range.to_range(),
+                horizontal_range: horizontal_range.to_range(),
+                iter,
+            }
+        }
+    }
+
+    // From https://poyopoyoyon.hatenablog.com/entry/2020/11/08/183212
+    impl<'a, I> Iterator for Adjacent2d<I>
+    where
+        I: Iterator<Item = &'a (isize, isize)>,
+    {
+        type Item = (usize, usize);
+
+        /// Returns (y, x)
+        fn next(&mut self) -> Option<(usize, usize)> {
+            while let Some((dy, dx)) = self.iter.next() {
+                let ny = self.current.0 + dy;
+                let nx = self.current.1 + dx;
+                if self.vertical_range.contains(&ny) && self.horizontal_range.contains(&nx) {
+                    return Some((ny as usize, nx as usize));
+                }
+            }
+            None
+        }
+    }
+
+    const A2D_4: &'static [(isize, isize)] = &[(-1, 0), (0, 1), (1, 0), (0, -1)];
+
+    /// Arguments
+    ///
+    /// * `current`: (y, x)
+    /// * `vertical_range`: y range
+    /// * `horizontal_range`: x range
+    pub fn adjacent2d_4neighbors(
+        current: (usize, usize),
+        vertical_range: impl adjacent_range::AdjacentRange,
+        horizontal_range: impl adjacent_range::AdjacentRange,
+    ) -> Adjacent2d<std::slice::Iter<'static, (isize, isize)>> {
+        Adjacent2d {
+            current: (current.0 as isize, current.1 as isize),
+            vertical_range: vertical_range.to_range(),
+            horizontal_range: horizontal_range.to_range(),
+            iter: A2D_4.iter(),
+        }
+    }
+
+    const A2D_8: &'static [(isize, isize)] = &[
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+        (-1, -1),
+    ];
+
+    /// Arguments
+    ///
+    /// * `current`: (y, x)
+    /// * `vertical_range`: y range
+    /// * `horizontal_range`: x range
+    pub fn adjacent2d_8neighbors(
+        current: (usize, usize),
+        vertical_range: impl adjacent_range::AdjacentRange,
+        horizontal_range: impl adjacent_range::AdjacentRange,
+    ) -> Adjacent2d<std::slice::Iter<'static, (isize, isize)>> {
+        Adjacent2d {
+            current: (current.0 as isize, current.1 as isize),
+            vertical_range: vertical_range.to_range(),
+            horizontal_range: horizontal_range.to_range(),
+            iter: A2D_8.iter(),
+        }
+    }
+}
+
 // -- end of helpers
 
 fn main() {
@@ -638,13 +777,81 @@ where
         input! {
             n: usize,
             k: usize,
-            s: [chars; n],
+            mut s: [chars; n],
         }
 
-        // FIXME: logic
+        // after checking editorial
 
-        // FIXME: print
-        println!();
+        let mut board_memo = HashSet::new();
+
+        struct Context<'a> {
+            n: usize,
+            s: &'a mut Vec<Vec<char>>,
+            board_memo: &'a mut HashSet<usize>,
+        }
+
+        // O(N^2) per search
+        fn dfs(board: usize, rest_reds: usize, ctx: &mut Context) -> usize {
+            if ctx.board_memo.contains(&board) {
+                dbg!("Dup.");
+                dbg!();
+                return 0;
+            }
+            ctx.board_memo.insert(board);
+
+            if rest_reds == 0 {
+                dbg!("Hit!");
+                dbg!();
+                return 1;
+            }
+            dbg!();
+
+            let mut next_ones = vec![];
+            for i in 0..ctx.n {
+                for j in 0..ctx.n {
+                    if ctx.s[i][j] == '.'
+                        && adjacent::adjacent2d_4neighbors((i, j), ..ctx.n, ..ctx.n)
+                            .any(|(i, j)| ctx.s[i][j] == '@')
+                    {
+                        next_ones.push((i, j));
+                    }
+                }
+            }
+
+            let mut counter = 0;
+            for (i, j) in next_ones {
+                ctx.s[i][j] = '@';
+
+                counter += dfs(board | 1 << i * ctx.n + j, rest_reds - 1, ctx);
+
+                ctx.s[i][j] = '.';
+            }
+
+            counter
+        }
+
+        let mut counter = 0;
+        for i in 0..n {
+            for j in 0..n {
+                if s[i][j] == '.' {
+                    s[i][j] = '@';
+
+                    counter += dfs(
+                        1 << i * n + j,
+                        k - 1,
+                        &mut Context {
+                            n,
+                            s: &mut s,
+                            board_memo: &mut board_memo,
+                        },
+                    ); // O(N^2 K)
+
+                    s[i][j] = '.';
+                }
+            }
+        } // O(N^4 K)
+
+        println!("{}", counter);
     }
 
     Ok(())
